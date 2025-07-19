4940103cc4fd7ee539da516f8ca4ae7752f187e8
Adam Griffin <ag@agriffin.me>
2025-07-19 13:32:55 -0700

    Layout changes

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,8 +3,8 @@ import { SocialLinks } from "./SocialLinks";
 
 export default function Footer() {
   return (
-    <footer className="block lg:mt-24 mt-16 text-[#1C1C1C] dark:text-[#D4D4D4]">
-      <time>© {new Date().getFullYear()}</time> {metaData.title}
+    <footer className="block text-[#1C1C1C] dark:text-[#D4D4D4] py-6">
+      <time className="text-sm">© {new Date().getFullYear()}</time> {metaData.title}
       <SocialLinks />
     </footer>
   );
