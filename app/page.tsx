93f0730aa14cd26f42ac3ff8cfda1ad0cb925472
Adam Griffin <ag@agriffin.me>
2025-07-19 13:33:45 -0700

    Placeholder text

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,29 @@ export default function Page() {
         height={160}
         priority
       />
-      <h1 className="mb-8 text-2xl font-medium">My portfolio</h1>
       <div className="prose prose-neutral dark:prose-invert">
-        <p>My portfolio</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac elit
+          tempus, suscipit eros a, gravida metus. Sed ac congue dui. Vivamus
+          consectetur consectetur posuere. Morbi rhoncus odio diam. Sed
+          ultricies risus risus, id vulputate purus luctus in. Donec ut molestie
+          odio, vitae ultricies enim.
+        </p>
+        <p>
+          In tempus volutpat iaculis. Donec felis turpis, dictum hendrerit
+          mollis in, mollis non mi. Quisque ut lacus a sem ultrices dignissim.
+          Ut malesuada purus in purus eleifend, et laoreet arcu varius. Mauris
+          aliquet cursus magna, sit amet venenatis ex malesuada egestas. Quisque
+          pharetra euismod nisl ut semper. Donec porttitor mattis augue, et
+          posuere ligula ornare a. Suspendisse leo mi, mollis cursus tempus sit
+          amet, lobortis ut quam. Curabitur sit amet dui augue.
+        </p>
+        <p>
+          Etiam ante nisl, suscipit ac suscipit vitae, molestie quis elit. Nam
+          auctor lacinia felis sed sollicitudin. Morbi vitae dignissim velit.
+          Cras a nulla pharetra dui auctor lacinia. Nam laoreet diam vitae leo
+          porta, et consequat quam viverra.
+        </p>
       </div>
     </section>
   );
