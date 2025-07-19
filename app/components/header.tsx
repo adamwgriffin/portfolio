d5a1342bbee8152499068f23c30ad6126bf87d5d
Adam Griffin <ag@agriffin.me>
2025-07-18 21:13:21 -0700

    Changed navbar to header

---
+++ app/components/header.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { ThemeSwitch } from "./theme-switch";
+
+const navItems = {
+  "/blog": { name: "Blog" },
+  "/projects": { name: "Projects" }
+};
+
+export function Header() {
+  return (
+    <header
+      className="flex flex-col md:flex-row md:items-center justify-between"
+    >
+      <div className="flex items-center">
+        <Link href="/">
+          <div className="text-3xl font-semibold">Adam Griffin</div>
+          <div>Software Engineer</div>
+        </Link>
+      </div>
+      <nav className="flex flex-row gap-4 mt-6 md:mt-0 md:ml-auto items-center">
+        {Object.entries(navItems).map(([path, { name }]) => (
+          <Link
+            key={path}
+            href={path}
+            className="transition-all hover:text-neutral-800
+              dark:hover:text-neutral-200 flex align-middle relative"
+          >
+            {name}
+          </Link>
+        ))}
+        <ThemeSwitch />
+      </nav>
+    </header>
+  );
+}
