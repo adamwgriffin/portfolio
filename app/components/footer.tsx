a169d8ebe15407fa311745cac468356ca6326e22
Adam Griffin <ag@agriffin.me>
2025-07-18 20:19:08 -0700

    Edited config

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,15 +1,6 @@
-"use client";
-
-import React from "react";
-import {
-  FaXTwitter,
-  FaGithub,
-  FaInstagram,
-  FaRss,
-  FaLinkedinIn,
-} from "react-icons/fa6";
-import { TbMailFilled } from "react-icons/tb";
 import { metaData, socialLinks } from "app/lib/config";
+import { FaGithub, FaLinkedinIn, FaRss } from "react-icons/fa6";
+import { TbMailFilled } from "react-icons/tb";
 
 const YEAR = new Date().getFullYear();
 
@@ -23,10 +14,11 @@ function SocialLink({ href, icon: Icon }) {
 
 function SocialLinks() {
   return (
-    <div className="flex text-lg gap-3.5 float-right transition-opacity duration-300 hover:opacity-90">
-      <SocialLink href={socialLinks.twitter} icon={FaXTwitter} />
+    <div
+      className="flex text-lg gap-3.5 float-right transition-opacity
+        duration-300 hover:opacity-90"
+    >
       <SocialLink href={socialLinks.github} icon={FaGithub} />
-      <SocialLink href={socialLinks.instagram} icon={FaInstagram} />
       <SocialLink href={socialLinks.linkedin} icon={FaLinkedinIn} />
       <SocialLink href={socialLinks.email} icon={TbMailFilled} />
       <a href="/rss.xml" target="_self">
@@ -39,23 +31,7 @@ function SocialLinks() {
 export default function Footer() {
   return (
     <small className="block lg:mt-24 mt-16 text-[#1C1C1C] dark:text-[#D4D4D4]">
-      <time>© {YEAR}</time>{" "}
-      <a
-        className="no-underline"
-        href={socialLinks.twitter}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {metaData.title}
-      </a>
-      <style jsx>{`
-        @media screen and (max-width: 480px) {
-          article {
-            padding-top: 2rem;
-            padding-bottom: 4rem;
-          }
-        }
-      `}</style>
+      <time>© {YEAR}</time> {metaData.title}
       <SocialLinks />
     </small>
   );
--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,16 +1,13 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  baseUrl: "https://adamgriffin.io/",
+  title: "Adam Griffin",
+  name: "Adam Griffin's Portfolio",
   ogImage: "/opengraph-image.png",
-  description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+  description: "A portfolio for Adam Griffin, Software Engineer."
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  github: "https://github.com/adamwgriffin",
+  linkedin: "https://www.linkedin.com/in/adamwgriffin/",
+  email: "mailto:job@adamgriffin.io"
 };
