3ddab6206dac2e3ac84686e2d8281f82a6e536f7
Adam Griffin <ag@agriffin.me>
2025-07-20 23:16:37 -0700

    Moved navigation into client component
    I want to be able to highlight the active link and it's difficult to do with a server component.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,9 @@
 import Link from "next/link";
 import { ThemeSwitch } from "./ThemeSwitch";
+import { Navigation } from "@/components/Navigation";
+import { Suspense } from "react";
 
-const navItems = {
-  "/": { name: "Home" },
-  "/experience": { name: "Experience" },
-  "/projects": { name: "Projects" },
-  "/contact": { name: "Contact" }
-};
-
-export function Header() {
+export async function Header() {
   return (
     <header className="pt-12 pb-6">
       <div className="text-center md:text-left">
@@ -30,20 +25,13 @@ export function Header() {
           className="flex flex-col items-center md:flex-row md:justify-between
             pt-4 gap-4"
         >
-          <nav className="flex gap-4">
-            {Object.entries(navItems).map(([path, { name }]) => (
-              <Link
-                key={path}
-                href={path}
-                className="transition-all hover:text-neutral-500
-                  dark:hover:text-neutral-200 flex align-middle relative"
-              >
-                {name}
-              </Link>
-            ))}
-          </nav>
+          <Suspense>
+            <Navigation />
+          </Suspense>
           <div className="h-7">
-            <ThemeSwitch />
+            <Suspense>
+              <ThemeSwitch />
+            </Suspense>
           </div>
         </div>
       </div>
---
+++ components/Navigation.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navItems = {
+  "/": { name: "Home" },
+  "/experience": { name: "Experience" },
+  "/projects": { name: "Projects" },
+  "/contact": { name: "Contact" }
+};
+
+export function Navigation() {
+  const currentPathname = usePathname();
+
+  return (
+    <nav className="flex gap-4">
+      {Object.entries(navItems).map(([path, { name }]) => (
+        <Link
+          key={path}
+          href={path}
+          aria-current={path === currentPathname ? "page" : false}
+          className="transition-all
+            not-aria-[current=page]:hover:text-neutral-500
+            dark:not-aria-[current=page]:hover:text-neutral-200 flex
+            align-middle relative aria-[current=page]:text-purple-500
+            dark:aria-[current=page]:text-fuchsia-300
+            aria-[current=page]:font-bold"
+        >
+          {name}
+        </Link>
+      ))}
+    </nav>
+  );
+}
