804f292c8b4e8ac9dac85a0c3459baf80ee6ca99
Adam Griffin <ag@agriffin.me>
2025-07-28 22:43:07 -0700

    Edited the front page

--- app/page.tsx
+++ app/page.tsx
@@ -17,11 +17,11 @@ export default function Page() {
       />
       <p>
         I&apos;m a web developer living in Seattle, WA. My tech stack has
-        primarily been Ruby on Rails, Postgres, React and Vue in my career. My
-        main role has been as a full stack developer but I&apos;ve always had a
-        strong interest in frontend development. I&apos;ve worked on several
-        real estate (Proptech) SaaS apps over the years, such as listing search
-        and a CRM.
+        primarily been Ruby on Rails, Postgres, React and Vue. My job has
+        usually been as a full stack developer but I&apos;ve always had a strong
+        interest in frontend development. I&apos;ve worked on several real
+        estate (Proptech) SaaS apps over the years, such as listing search and a
+        CRM.
       </p>
       <p>
         In my spare time I mostly use Typescript with Next.js and Node. I&apos;m
@@ -29,22 +29,15 @@ export default function Page() {
         <a href="https://koajs.com/#introduction" target="_blank">
           Koa
         </a>{" "}
-        as a backend framework too. I use MongoDB a lot because I find it easy
-        to iterate quickly with it, but I&apos;m also a big fan of Postgres. You
-        can check out <Link href="/projects">my projects</Link>, or my{" "}
+        as a backend framework too. I use MongoDB a lot but I&apos;m also a big
+        fan of Postgres. Occasionally I dabble in Go and the Gin web framework.
+        I enjoy the emphasis on simplicity in the language but I have yet to
+        truly become a &quot;Gopher.&quot; Maybe someday. You can check out{" "}
+        <Link href="/projects">my projects</Link>, or my{" "}
         <a href={socialLinks.github} target="_blank" rel="noopener noreferrer">
           Github
         </a>
         , to see some of the stuff I work on.
-      </p>
-      <p>
-        Occasionally I dabble in Go and the Gin web framework. I enjoy the
-        emphasis on simplicity in the language but I have yet to truly become a
-        &quot;Gopher.&quot; Maybe someday. I&apos;ve also been looking at Kotlin
-        but have yet to dive in. Recently, I&apos;ve taken more of an interest
-        in DevOps. I&apos;ve taken a class on Kubernetes and written a few
-        things with it. I&apos;m also interested in IAC tools like Terraform. So
-        much tech to learn, so little time!
       </p>
       <p>
         If you have a role you think I might be interested in, hit me up via{" "}
