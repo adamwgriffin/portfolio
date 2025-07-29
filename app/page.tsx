cebf7db126ed8b7a44c4f5274c7e7569c368f2f9
Adam Griffin <ag@agriffin.me>
2025-07-29 09:58:44 -0700

    Edited about page

--- app/page.tsx
+++ app/page.tsx
@@ -17,29 +17,30 @@ export default function Page() {
         priority
       />
       <p>
-        I&apos;m a web developer living in Seattle, WA. My tech stack has
-        primarily been Ruby on Rails, Postgres, React and Vue. My job has
-        usually been as a full stack developer but I&apos;ve always had a strong
-        interest in frontend development. I&apos;ve worked on several real
-        estate (Proptech) SaaS apps over the years, such as listing search and a
-        CRM.
-      </p>
-      <p>
-        In my spare time I mostly use Typescript with Next.js and Node. I&apos;m
-        fond of{" "}
-        <a href="https://koajs.com/#introduction" target="_blank">
+        I&apos;m a web developer living in Seattle, WA. In my spare time I
+        mostly use Typescript with Next.js and Node. I&apos;m fond of{" "}
+        <a href="https://koajs.com/" target="_blank">
           Koa
         </a>{" "}
-        as a backend framework too. I use MongoDB a lot but I&apos;m also a big
-        fan of Postgres. Occasionally I dabble in Go and the Gin web framework.
-        I enjoy the emphasis on simplicity in the language but I have yet to
-        truly become a &quot;Gopher.&quot; Maybe someday. You can check out{" "}
+        as a backend framework too. I use MongoDB a lot but I&apos;m also a fan
+        of Postgres. Occasionally I dabble in Go and the Gin web framework. I
+        enjoy the emphasis on simplicity in the language but I have yet to truly
+        become a &quot;Gopher.&quot; Maybe someday. You can check out{" "}
         <Link href="/projects">my projects</Link>, or my{" "}
         <a href={socialLinks.github} target="_blank" rel="noopener noreferrer">
           Github
         </a>
         , to see some of the stuff I work on.
       </p>
+
+      <p>
+        At work my tech stack has primarily been Ruby on Rails, Postgres and
+        React/Angular/Vue. My job has been as a full stack developer but
+        I&apos;ve always had a strong interest in frontend development.
+        I&apos;ve worked on several real estate (Proptech) SaaS apps over the
+        years, such as listing search and a CRM.
+      </p>
+
       <p>
         If you have a role you think I might be interested in, hit me up via{" "}
         <a href={socialLinks.email} target="_blank" rel="noopener noreferrer">
