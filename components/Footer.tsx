d04bc1f8a9667e1fe1c0a88caaa98fd5a20989cc
Adam Griffin <ag@agriffin.me>
2025-07-29 19:41:34 -0700

    Center copyright text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
   return (
     <footer className="flex justify-between text-zinc-600 dark:text-zinc-300
       py-12">
-      <AnimatedFadeInDiv className="text-xs">
+      <AnimatedFadeInDiv className="flex items-center text-xs">
         <time>© {new Date().getFullYear()}</time> {metaData.title}
       </AnimatedFadeInDiv>
       <AnimatedFadeInDiv className="flex items-center gap-4">
