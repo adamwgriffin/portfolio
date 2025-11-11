058d0f86b11d12a57b81e059470a2ade62f7ef0c
Adam Griffin <ag@agriffin.me>
2025-11-10 20:27:24 -0800

    Removed accordion widget
    It seems better to just be able to read everything at a glance rather than having to constantly click the accordion menus.

--- app/stack/page.tsx
+++ app/stack/page.tsx
@@ -1,6 +1,4 @@
-import { AccordianItemWithIcon } from "@/components/AccordianItemWithIcon";
 import { AnimatedSection } from "@/components/AnimatedSection";
-import { Accordion } from "@/components/motion-primitives/accordion";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -18,109 +16,102 @@ export default function Page() {
         </p>
       </div>
 
-      <Accordion className="flex w-full flex-col">
-        <AccordianItemWithIcon value="languages" title="Languages">
-          <div className="px-6 pt-2">
-            <ul>
-              <li>Typescript</li>
-              <li>Javascript</li>
-              <li>Coffeescript</li>
-              <li>Ruby</li>
-              <li>SQL</li>
-              <li>CSS</li>
-              <li>Sass</li>
-            </ul>
-          </div>
-        </AccordianItemWithIcon>
+      <div className="md:columns-2">
+        <div className="pb-6">
+          <h1 className="font-medium text-2xl mb-4">Languages</h1>
+          <ul>
+            <li>Typescript</li>
+            <li>Javascript</li>
+            <li>Coffeescript</li>
+            <li>Ruby</li>
+            <li>SQL</li>
+            <li>CSS</li>
+            <li>Sass</li>
+          </ul>
+        </div>
 
-        <AccordianItemWithIcon value="frameworks" title="Frameworks">
-          <div className="px-6 pt-2">
-            <ul>
-              <li>React</li>
-              <li>Next.js</li>
-              <li>Angular</li>
-              <li>Vue</li>
-              <li>Ruby on Rails</li>
-              <li>Koa</li>
-              <li>Express</li>
-              <li>Backbone</li>
-            </ul>
-          </div>
-        </AccordianItemWithIcon>
+        <div className="pb-6">
+          <h1 className="font-medium text-2xl mb-4">Frameworks</h1>
+          <ul>
+            <li>React</li>
+            <li>Next.js</li>
+            <li>Angular</li>
+            <li>Vue</li>
+            <li>Ruby on Rails</li>
+            <li>Koa</li>
+            <li>Express</li>
+            <li>Backbone</li>
+          </ul>
+        </div>
 
-        <AccordianItemWithIcon value="data" title="Data">
-          <div className="px-6 pt-2">
-            <ul>
-              <li>PostgreSQL/PostGIS</li>
-              <li>MongoDB</li>
-              <li>Redis</li>
-            </ul>
-          </div>
-        </AccordianItemWithIcon>
+        <div className="pb-6">
+          <h1 className="font-medium text-2xl mb-4">Data</h1>
+          <ul>
+            <li>PostgreSQL/PostGIS</li>
+            <li>MongoDB</li>
+            <li>Redis</li>
+          </ul>
+        </div>
 
-        <AccordianItemWithIcon value="tools" title="Tools">
-          <div className="px-6 pt-2">
-            <ul>
-              <li>Docker/Docker Compose</li>
-              <li>Git</li>
-              <li>Jest</li>
-              <li>Mocha/Chai</li>
-              <li>Rspec</li>
-              <li>PDFreactor (service that converts HTML document to PDF)</li>
-              <li>Jenkins</li>
-            </ul>
-          </div>
-        </AccordianItemWithIcon>
+        <div className="pb-6">
+          <h1 className="font-medium text-2xl mb-4">Tools</h1>
+          <ul>
+            <li>Docker/Docker Compose</li>
+            <li>Git</li>
+            <li>Jest</li>
+            <li>Mocha/Chai</li>
+            <li>Rspec</li>
+            <li>PDFreactor (service that converts HTML document to PDF)</li>
+            <li>Jenkins</li>
+          </ul>
+        </div>
 
-        <AccordianItemWithIcon value="libraries" title="Libraries">
-          <div className="px-6 pt-2">
-            <ul>
-              <li>React Query</li>
-              <li>Zod</li>
-              <li>ShadCN</li>
-              <li>Tailwind</li>
-              <li>Prisma</li>
-              <li>Mongoose</li>
-              <li>D3</li>
-              <li>Redux/Redux Toolkit</li>
-              <li>RxJS</li>
-              <li>jQuery</li>
-              <li>NextAuth (Auth.js)</li>
-              <li>Turf (Javascript geospatial library)</li>
-              <li>Faker</li>
-              <li>Bootstrap</li>
-              <li>Materialize</li>
-            </ul>
-          </div>
-        </AccordianItemWithIcon>
+        <div className="pb-6">
+          <h1 className="font-medium text-2xl mb-4">Libraries</h1>
+          <ul>
+            <li>React Query</li>
+            <li>Zod</li>
+            <li>ShadCN</li>
+            <li>Tailwind</li>
+            <li>Prisma</li>
+            <li>Mongoose</li>
+            <li>D3</li>
+            <li>Redux/Redux Toolkit</li>
+            <li>RxJS</li>
+            <li>jQuery</li>
+            <li>NextAuth (Auth.js)</li>
+            <li>Turf (Javascript geospatial library)</li>
+            <li>Faker</li>
+            <li>Bootstrap</li>
+            <li>Materialize</li>
+          </ul>
+        </div>
 
-        <AccordianItemWithIcon value="apis" title="Third-Party APIs">
-          <div className="px-6 pt-2">
-            <ul>
-              <li>Facebook Marketing API</li>
-              <li>Google Maps API</li>
-              <li>Google People API</li>
-              <li>Exchange Web Services (EWS)</li>
-              <li>Braintree Payment API</li>
-            </ul>
-          </div>
-        </AccordianItemWithIcon>
+        <div className="pb-6">
+          <h1 className="font-medium text-2xl mb-4">Third-Party APIs</h1>
+          <ul>
+            <li>Facebook Marketing API</li>
+            <li>Google Maps API</li>
+            <li>Google People API</li>
+            <li>Exchange Web Services (EWS)</li>
+            <li>Braintree Payment API</li>
+          </ul>
+        </div>
 
-        <AccordianItemWithIcon value="apps" title="Apps">
-          <div className="px-6 pt-2">
-            <ul>
-              <li>Github</li>
-              <li>Datadog</li>
-              <li>BuildKite</li>
-              <li>Kibana</li>
-              <li>Figma</li>
-              <li>Vercel</li>
-              <li>Zendesk</li>
-              <li>Jira</li>
-            </ul>
-          </div>
-        </AccordianItemWithIcon>
-      </Accordion>
+        <div className="pb-6">
+          <h1 className="font-medium text-2xl mb-4">Apps</h1>
+          <ul>
+            <li>Github</li>
+            <li>Datadog</li>
+            <li>BuildKite</li>
+            <li>Kibana</li>
+            <li>Figma</li>
+            <li>Vercel</li>
+            <li>Zendesk</li>
+            <li>Jira</li>
+          </ul>
+        </div>
+      </div>
     </AnimatedSection>
   );
 }
